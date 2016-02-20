6c896e887cc0ea548807702e870a8480407f0035
Huber <huber.gerard1@gmail.com>
2016-02-20 13:20:56 +0100

    Mise en place projet

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the websemApp
+ */
+angular.module('websemApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websemApp
+ */
+angular.module('websemApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
